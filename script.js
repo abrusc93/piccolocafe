45c76fa58a21b2aeff2396c3511d158b181973d5
abrusc93 <abrusc93@students.rowan.edu>
2022-08-24 19:41:54 -0400

    Update Events Page
    Added Scroll Reveal

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $('.navbar-nav>.section-link>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+ScrollReveal().reveal('.reveal', { 
+    delay: 200
+});
+
+ScrollReveal().reveal('.reveal-delay', { 
+    delay: 600
+});
+
+ScrollReveal().reveal('.reveal-slow', { 
+    delay: 300,
+    duration: 3000,
+    easing: 'ease-in'
+});
