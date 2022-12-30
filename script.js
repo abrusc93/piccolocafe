855cdbfc170ac00746a33711ff443b28d599ac80
abrusc93 <abrusc93@students.rowan.edu>
2022-12-30 10:58:43 -0500

    Improvements
    Event flyers are displayed in a modal instead of a separate html page; Rounded nav dropdown links.

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ if($(window).width()>768){
 }
 
 $('.menu-page *').addClass('animate__animated animate__fadeIn');
+
+const exampleModal = document.getElementById('photoDisplayModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const imgSrc1 = button.getAttribute('data-bs-image1')
+  const imgSrc2 = button.getAttribute('data-bs-image2')
+  // Update the modal's img src attribute.
+  document.getElementById("modalImage1").src = imgSrc1;
+  document.getElementById("modalImage2").src = imgSrc2;
+})
